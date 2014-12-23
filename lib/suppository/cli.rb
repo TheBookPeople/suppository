b44a508e444ba87fafe04facac659b5cf2fc58d1
William Griffiths <william.griffiths@thebookpeople.co.uk>
2014-12-23 21:40:31 +0000

    integrate add package to cli

--- lib/suppository/cli.rb
+++ lib/suppository/cli.rb
@@ -7,7 +7,19 @@ module Suppository
         puts "Suppository Version #{Suppository::VERSION}"
         return
       end
-      Suppository::CreateRepository.new(args[1]).run
+      
+      if args.first == 'create'
+        repository = Suppository::Repository.new(args[1])
+        Suppository::CreateRepository.new(repository).run
+        return
+      end
+      
+      if args.first == 'add'
+        repository = Suppository::Repository.new(args[1])
+        Suppository::AddPackage.new(repository, args[2]).run
+        return
+      end
+      
     end
   end
 end
--- spec/suppository/cli_spec.rb
+++ spec/suppository/cli_spec.rb
@@ -6,15 +6,31 @@ require 'suppository/create_repository'
 
 describe Suppository::CLI do
   
+  before(:each) do
+    @path = '/tmp/repo123'
+    @repository = double(Suppository::Repository)
+  end
+  
   it "shows current version" do    
-    expect{Suppository::CLI.run(["version"])}.to output("Suppository Version 0.0.1\n").to_stdout   
+    expect{Suppository::CLI.run(['version'])}.to output("Suppository Version 0.0.1\n").to_stdout   
   end
   
   it "can create new repository" do    
+    expect(Suppository::Repository).to receive(:new).with(@path) {@repository}
+    
     creator = double(Suppository::CreateRepository)
-    expect(Suppository::CreateRepository).to receive(:new).with("/tmp/repo123") {creator}
+    expect(Suppository::CreateRepository).to receive(:new).with(@repository) {creator}
     expect(creator).to receive(:run)
-    Suppository::CLI.run(["create", "/tmp/repo123"])
+    Suppository::CLI.run(['create', @path])
+  end
+  
+  it "can add deb to existing repository" do 
+    expect(Suppository::Repository).to receive(:new).with(@path) {@repository}
+    
+    adder = double(Suppository::AddPackage)
+    expect(Suppository::AddPackage).to receive(:new).with(@repository, '/tmp/example.deb') {adder}
+    expect(adder).to receive(:run)   
+    Suppository::CLI.run(['add', @path ,'/tmp/example.deb'])
   end
   
   

b426ce0610af695527e43cbd91cce1f3daa83bfb
Luke Farrar <luke.farrar@thebookpeople.co.uk>
2015-01-09 15:18:37 +0000

    Revert silly checksum grouping mistake.

--- lib/suppository/release.rb
+++ lib/suppository/release.rb
@@ -38,17 +38,20 @@ module Suppository
       result << "Components: #{components}\n"
       result << "Date: #{Time.new.strftime('%a, %d %b %Y %H:%M:%S %Z')}\n"
       packages = Dir.glob("#{@dist_path}/*/*/Packages*")
-
-      packages.each { |f| result << puts_hash('MD5Sum', f, Digest::MD5.file(f)) }
-      packages.each { |f| result << puts_hash('SHA1', f, Digest::SHA1.file(f)) }
-      packages.each { |f| result << puts_hash('SHA256', f, Digest::SHA256.file(f)) }
-      packages.each { |f| result << puts_hash('SHA512', f, Digest::SHA512.file(f)) }
+      result << "MD5Sum:\n"
+      packages.each { |f| result << puts_hash(f, Digest::MD5.file(f)) }
+      result << "SHA1:\n"
+      packages.each { |f| result << puts_hash(f, Digest::SHA1.file(f)) }
+      result << "SHA256:\n"
+      packages.each { |f| result << puts_hash(f, Digest::SHA256.file(f)) }
+      result << "SHA512:\n"
+      packages.each { |f| result << puts_hash(f, Digest::SHA512.file(f)) }
       result
     end
 
-    def puts_hash(key, f, hash)
+    def puts_hash(f, hash)
       relative = f.split(@dist_path).pop[1..-1]
-      sprintf("%s:\n %s %17d %s\n", key, hash, File.size(f), relative)
+      sprintf(" %s %17d %s\n", hash, File.size(f), relative)
     end
   end
 end
